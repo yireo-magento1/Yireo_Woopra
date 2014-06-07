f9a456e2a7912a327a8b1c84da828869296bbbfa
Jisse Reitsma <info@yireo.com>
2014-06-07 10:14:50 +0200

    Initial commit

---
+++ source/app/code/community/Yireo/Woopra/Helper/Data.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Woopra plugin for Magento 
+ *
+ * @category    design_default
+ * @package     Yireo_Woopra
+ * @author      Yireo (http://www.yireo.com/)
+ * @copyright   Copyright (C) 2014 Yireo (http://www.yireo.com/)
+ * @license     Open Source License (OSL v3)
+ */
+
+class Yireo_Woopra_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    public function getApiKey()
+    {
+        return Mage::getStoreConfig('woopra/settings/website_id');
+    }
+
+    public function enabled()
+    {
+        return (bool)Mage::getStoreConfig('woopra/settings/active');
+    }
+
+    public function test()
+    {
+        return (bool)Mage::getStoreConfig('woopra/settings/test');
+    }
+}
